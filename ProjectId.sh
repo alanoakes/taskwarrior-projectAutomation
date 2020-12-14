e532f811cf0acfc757f562bc535fa4952bc515b2
Parker <38483827+alanoakes@users.noreply.github.com>
2020-12-14 17:34:21 -0600

    Create ProjectId.sh

---
+++ ProjectId.sh
@@ -0,0 +1,19 @@
+#!usr/bin/env bash
+# + ------------------------------------------------------------------------- +
+# ┏━┓┏━┓┏━┓╻┏ ┏━╸┏━┓ | Parker Oakes                                           +
+# ┣━┛┃ ┃┣━┫┣┻┓┣╸ ┗━┓ | alan.p.oakes@gmail.com                                 +
+# ╹  ┗━┛╹ ╹╹ ╹┗━╸┗━┛ | github.com/alanoakes | parkeroakes.io                  +
+# + ------------------------------------------------------------------------- +
+# File Description:
+# Generate Randomized Project ID Numbers with timestamp for Data Security
+
+ProjDate="P$(date +%Y%m%d%H)-"
+Proj=""
+until [ ${#Proj} -eq '10' ]; do
+  for x in $(echo {a..z}$(shuf -i 1-10 -r -n 1) | tr -d '\n' | tr -d ' '); do
+	for y in ${x:$((RANDOM%52+1)):1}; do
+      Proj="$Proj$y"
+    done
+  done
+done
+echo "$ProjDate$Proj"
