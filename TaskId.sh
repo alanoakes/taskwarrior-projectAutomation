d2897ff0f4a9908e5a4da264511826aef152c559
parker <alan.p.oakes@gmail.com>
2020-12-14 21:21:11 -0600

    updated script.sh with automated project id and started task id

--- TaskId.sh
+++ TaskId.sh
@@ -1,19 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # + ------------------------------------------------------------------------- +
 # ┏━┓┏━┓┏━┓╻┏ ┏━╸┏━┓ | Parker Oakes                                           +
 # ┣━┛┃ ┃┣━┫┣┻┓┣╸ ┗━┓ | alan.p.oakes@gmail.com                                 +
 # ╹  ┗━┛╹ ╹╹ ╹┗━╸┗━┛ | github.com/alanoakes | parkeroakes.io                  +
 # + ------------------------------------------------------------------------- +
 # File Description:
-# Generate Randomized Project ID Numbers with timestamp for Data Security
-ProjDate="P$(date +%Y%m%d%H%M)-"
-ProjId=""
-until [ ${#ProjId} -eq '10' ]; do
-  for x in $(echo {a..z}$(shuf -i 1-10 -r -n 1) | tr -d '\n' | tr -d ' '); do
-    for y in ${x:$((RANDOM%52+1)):1}; do
-      ProjId="$ProjId$y"
-    done
-  done
-done
-ProjectId="$ProjDate$ProjId"
-echo $ProjectId
+# Automate trouble tickets based on phase and project with timestamp
+declare -i taId="$(task active | grep -o -E '[0-9]+' | head -n 1)"
+taUUID="$(task _get ${taId}.uuid | head -c 8)"
+taProjId="$(task _get ${taId}.tags)"
+taDate="$(date +%Y%m%d%H%M)"
+echo $taProjId$taUUID$taDate
--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# + ------------------------------------------------------------------------- +
+# ┏━┓┏━┓┏━┓╻┏ ┏━╸┏━┓ | Parker Oakes                                           +
+# ┣━┛┃ ┃┣━┫┣┻┓┣╸ ┗━┓ | alan.p.oakes@gmail.com                                 +
+# ╹  ┗━┛╹ ╹╹ ╹┗━╸┗━┛ | github.com/alanoakes | parkeroakes.io                  +
+# + ------------------------------------------------------------------------- +
+# File Description:
+# Automate SDLC Project setup with secure ID's 
+
 
 echo 'Initiating new software project...'
 while :; do
@@ -7,27 +15,42 @@ while :; do
 	# determine if a bash variable is empty
 	if [[ -z "$SoftwareProj" ]]
 	then 
-		echo 'Your entry has no characters. Please re-enter project name or press Ctrl + c to exit project setup.'
+		echo "Your entry has no characters. Please re-enter project name or press Ctrl + c to exit project setup."
 	else
-		echo 'Building \$SoftwareProj project in taskwarrior ...'
 
-		cmd01=`task add pro:$SoftwareProj +Software 'Planning'`
+    # Generate encrypted project id:
+    ProjDate="P$(date +%Y%m%d%H%M)_"
+    ProjId=""
+    until [ ${#ProjId} -eq '10' ]; do
+      for x in $(echo {a..z}$(shuf -i 1-10 -r -n 1) | tr -d '\n' | tr -d ' '); do
+        for y in ${x:$((RANDOM%52+1)):1}; do
+          ProjId="$ProjId$y"
+        done
+      done
+    done
+    ProjectId="+$ProjDate$ProjId"
+
+    echo "Building $SoftwareProj in taskwarrior with project ID: $ProjDate$ProjId"
+
+    # Generate new software project
+		cmd01=`task add pro:$SoftwareProj $ProjectId 'Planning'`
 		id01=`echo $cmd01 | grep -o -E '[0-9]+'`
-		cmd02=`task add pro:$SoftwareProj +Software 'Design' depends:$id01`
+		cmd02=`task add pro:$SoftwareProj $ProjectId 'Design' depends:$id01`
 		id02=`echo $cmd02 | grep -o -E '[0-9]+'`
-		cmd03=`task add pro:$SoftwareProj +Software 'Development' depends:$id02`
+		cmd03=`task add pro:$SoftwareProj $ProjectId 'Development' depends:$id02`
 		id03=`echo $cmd03 | grep -o -E '[0-9]+'`
-		cmd04=`task add pro:$SoftwareProj +Software 'Testing' depends:$id03`
+		cmd04=`task add pro:$SoftwareProj $ProjectId 'Testing' depends:$id03`
 		id04=`echo $cmd04 | grep -o -E '[0-9]+'`
-		cmd05=`task add pro:$SoftwareProj +Software 'Deployment' depends:$id04`
+		cmd05=`task add pro:$SoftwareProj $ProjectId 'Deployment' depends:$id04`
 		id05=`echo $cmd05 | grep -o -E '[0-9]+'`
-		cmd06=`task add pro:$SoftwareProj +Software 'Maintenance' depends:$id05`
+		cmd06=`task add pro:$SoftwareProj $ProjectId 'Maintenance' depends:$id05`
 		id06=`echo $cmd06 | grep -o -E '[0-9]+'`
-		# clear (unset) project variables so they are not incorrectly reassigned
+
+    # clear (unset) project variables so they are not incorrectly reassigned
 		SoftwareProj=""
 
 	fi
 	# que to build another project
-	read -r -p 'Do you want to build another software project? (type "e" to exit or "n" to resume): '
-	[[ "${REPLY}" == 'e' ]] && break
+	read -r -p 'Do you want to build another software project? (type "n" to exit or "y" to resume): '
+	[[ "${REPLY}" == 'n' ]] && break
 done
